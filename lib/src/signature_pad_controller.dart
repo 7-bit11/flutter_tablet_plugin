3d93ef3b146fb7ddd1301a98362eb2dbf0a0ca40
7-bit11 <210014468@qq.com>
2025-01-17 15:07:18 +0800

    ✨ feat(抽离):

--- lib/src/signature_pad_controller.dart
+++ lib/src/signature_pad_controller.dart
@@ -3,9 +3,43 @@ import 'package:flutter/material.dart';
 class SignaturePadController extends ChangeNotifier {
   SignaturePadController();
 
-  // 存储用户绘制的路径
+  /// 存储用户绘制的路径
   ValueNotifier<List<Path>> paths = ValueNotifier([]);
 
-  // 存储每个路径的绘制点
+  /// 存储每个路径的绘制点
   ValueNotifier<List<List<Offset>>> points = ValueNotifier([]);
+
+  ///  文本颜色
+  ValueNotifier<Color> textColor = ValueNotifier(Colors.black);
+
+  /// 画笔大小
+  ValueNotifier<double> penSize = ValueNotifier(5.0);
+
+  /// 当用户开始触摸屏幕时，创建新的路径和新的点列表
+  void onPanStart(DragStartDetails details) {
+    paths.value.add(
+        Path()..moveTo(details.localPosition.dx, details.localPosition.dy));
+    points.value.add([details.localPosition]);
+    notifyListeners();
+  }
+
+  /// 当用户触摸时，完成当前路径的绘制
+  void onPanUpdate(DragUpdateDetails details) {
+    // 获取当前正在绘制的路径和点列表
+    final int index = paths.value.length - 1;
+    final Path path = paths.value[index];
+    final List<Offset> point = points.value[index];
+    // 将当前触摸点添加到点列表中
+    point.add(details.localPosition);
+    // 从上次的点到当前点绘制直线
+    if (point.length >= 2) {
+      //_paths[index] = path;
+      path.quadraticBezierTo(
+        point[points.value.length - 2].dx,
+        point[points.value.length - 2].dy,
+        (point[points.value.length - 2].dx + details.localPosition.dx) / 2,
+        (point[points.value.length - 2].dy + details.localPosition.dy) / 2,
+      );
+    }
+  }
 }
